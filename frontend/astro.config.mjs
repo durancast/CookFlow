9f82b63220da9733fa94b1637277350b30d711bf
durancast <alexduranc22@gmail.com>
2026-03-17 17:28:46 +0100

    mejorar interfaz y añadir react

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     //@ts-ignore
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
