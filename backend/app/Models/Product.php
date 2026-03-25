60469963cfd560cbcaf473ecddd33b6e80caf6d5
luigiGit25 <luispincay88.lp@gmail.com>
2026-03-25 15:03:42 +0100

    Migraciones de order y order_items. Models configurados tmb

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Product extends Model
 {
     // 1. Añadir esta línea para evitar el error de SQL
-    public $timestamps = false; 
+    public $timestamps = false;
 
     protected $fillable = ['name', 'description', 'price', 'image', 'category_id'];
 
@@ -16,4 +16,10 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
--- backend/database/migrations/2026_03_09_184740_create_products_table.php
+++ backend/database/migrations/2026_03_09_184740_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->decimal('price', 8, 2);
-            $table->text('description')->nullable(); 
+            $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
         });
